fa672b8ca128184493b3abff68ca23eaf866c35a
stephenottaway <stephenott14@icloud.com>
2024-05-14 10:26:07 -0700

    fixed wind vane code and updated final code

--- Code/Final_Code/Final_Code.ino
+++ Code/Final_Code/Final_Code.ino
@@ -1,28 +1,66 @@
+#include <ModbusMaster.h>
+#include <SoftwareSerial.h>
 #include "HX711.h"
+
 #define SPEED_PIN 1
 #define CLK 2
 #define DAT 3
+#define MAX485_DE_RE 4
+#define MS_RX 10
+#define MS_TX 11
+#define WV_SLAVE_ADDRESS 2
+#define WD_DEGREE_REGISTER_ADDRESS 0x0000
+#define OFFSET_FACTOR 4294958576 
+#define SCALE_FACTOR -7*134.138458 // factor of -7 there to read correct weights, may need to be changed
+#define BAUD_RATE 9600
 #define DELAY_MSEC 3000
-#define OFFSET_FACTOR 4294958576 // scale offset factor obtained from scale calibration code
-#define SCALE_FACTOR -7*134.138458 // negative of scale SCALE factor obtained from scale calibration code to account for outputted weights being negative, and multiplied by seven because 
-// the reading is off for whatever reason.  Will try to fix later, but this is a working solution for now.
 
+
+ModbusMaster node;
 HX711 scale;
+
+SoftwareSerial mySerial(MS_RX,MS_TX); 
 
 double weightInPounds = 0;
 double wind_speed = 0;
 int analog_to_digital = 0;
 
-void setup() {
-  Serial.begin(9600);
+void preTransmission()
+{
+  digitalWrite(MAX485_DE_RE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_DE_RE, 0);
+}
+
+void setup() 
+{
+  pinMode(MAX485_DE_RE, OUTPUT);
+  digitalWrite(MAX485_DE_RE, 0);
+  Serial.begin(BAUD_RATE);
+  mySerial.begin(BAUD_RATE);
+  node.begin(WV_SLAVE_ADDRESS, mySerial);
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
   scale.begin(DAT,CLK);
   scale.set_offset(OFFSET_FACTOR);
   scale.set_scale(SCALE_FACTOR);
-  scale.tare(); // Assuming there is no weight on the scale at start up, reset the scale to 0. WILL LIKELY NEED TO GET RID OF LATER TO ACCOUNT FOR 
-  // LATENT TENSION ON STRAIN GAUGE DURING THE SETUP
+  scale.tare(); // This line is only necessary if nothing is on the scale during setup
 }
 
-void loop() {
+void loop() 
+{
+  uint8_t result;
+  result = node.readHoldingRegisters(WD_DEGREE_REGISTER_ADDRESS, 1);  // only reading 1 register address
+  if (result == node.ku8MBSuccess)
+  {
+    Serial.print("Wind direction (0 is due north): "); 
+    Serial.print(node.getResponseBuffer(0x0)/10.0f); 
+    Serial.println(" degrees");
+  }
+  delay(DELAY_MSEC);
   weightInPounds = scale.get_units();
   analog_to_digital = analogRead(SPEED_PIN); 
   wind_speed = (32.2/348)*(analog_to_digital-85);
--- Code/Wind_Direction/Wind_Direction_Updated/Wind_Direction_Updated.ino
+++ Code/Wind_Direction/Wind_Direction_Updated/Wind_Direction_Updated.ino
@@ -1,42 +1,45 @@
 #include <ModbusMaster.h>
+#include <SoftwareSerial.h>
 
 /* 
     Using the RS485 transceiver module, Rx/Tx is hooked up to the hardware
     serial port at 'Serial" to DI (driver input, connected to Tx) and RO 
     (receiver ouput, connected to Rx). 
-    The data enable and receiver enable pins are connected as follows:
+    DE and RE are shorted together and connected to pin 4
 */
-#define MAX485_DE 4
-#define MAX485_RE_NEG 5
+// shorting DE and RE together 
+#define MAX485_DE_RE 4
 
 // instantiate the ModbusMaster object
 ModbusMaster node;
 
+// configure serial communication pins for wind vane
+SoftwareSerial mySerial(10,11); // RX, TX
+
 void preTransmission()
 {
-  digitalWrite(MAX485_RE_NEG, 1);
-  digitalWrite(MAX485_DE, 1);
+  digitalWrite(MAX485_DE_RE, 1);
 }
 
 void postTransmission() 
 {
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
+  digitalWrite(MAX485_DE_RE, 0);
 }
 
 void setup()
 {
-  pinMode(MAX485_RE_NEG, OUTPUT);
-  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE_RE, OUTPUT);
   // initialize in receive mode
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
+  digitalWrite(MAX485_DE_RE, 0);
 
-  // Modbus communication runs at 9600 baud
+  // Serial communication for printing to serial monitor
   Serial.begin(9600);
 
+  // Serial communication for transmitting and receiving data from wind vane
+  mySerial.begin(9600);
+
   // Modbus slave ID of wind vane is 2 
-  node.begin(2, Serial);
+  node.begin(2, mySerial);
 
   // Callbacks allow us to configure the RS485 transceiver correctly
   node.preTransmission(preTransmission);
@@ -47,14 +50,14 @@ void setup()
 void loop() 
 {
   uint8_t result;
-  uint16_t data[7]; // slave response frame for buffer is 7 bytes according to data sheet
 
   // read wind direction value at register 0x0000
   result = node.readHoldingRegisters(0x0000, 1);
   if (result == node.ku8MBSuccess) 
   {
-    //Serial.println("Wind direction of 360 degrees (hex): "); 
-    Serial.println(node.getResponseBuffer(0x0));  
+    Serial.print("Wind direction (0 is due north): "); 
+    Serial.print(node.getResponseBuffer(0x0)/10.0f); 
+    Serial.println(" degrees");
   }
 
   Serial.println();
